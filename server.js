66815a7ce4cf483a37731ac5fbe49b595c8bde7a
Richard <richard.yang.tong@gmail.com>
2021-03-25 09:27:00 -0400

    Added basic Express server.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// HTTP request logger
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+    console.log(`==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT} in your browser.`);
+});
