9946216e10bb4a620a0acea620b45620dd670027
Michael Wong <michael.w228@gmail.com>
2021-03-29 12:51:49 -0400

    initial carousel

--- client/src/components/ShopPage/index.js
+++ client/src/components/ShopPage/index.js
@@ -1,9 +1,41 @@
+import "./shop.css"
+
 function ShopPage() {
   return(
-    <div>
-      Shop Page
-    </div>
+    <>
+      <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img src="https://www.hermanmiller.com/content/dam/hmicom/page_assets/products/categories/gaming/it_cmp_gaming_motia_gaming_table.jpg" class="d-block w-100" alt="desk"/> 
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Standing Desk</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img src="https://robbreport.com/wp-content/uploads/2020/09/https___hypebeast.jpg?w=1000" class="d-block w-100" alt="chair"/>
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+
   )
 }
 
-export default ShopPage;
+export default ShopPage
