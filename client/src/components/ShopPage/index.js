60bb1c8a91a8df900e0a43498ce075c083e77353
Michael Wong <michael.w228@gmail.com>
2021-03-29 13:26:15 -0400

    changed design of carousel

--- client/src/components/ShopPage/index.js
+++ client/src/components/ShopPage/index.js
@@ -1,12 +1,28 @@
 import "./shop.css"
+import { useEffect, useState } from 'react'
+
 
 function ShopPage() {
+
+  // const [products, setProducts] = useState([])
+  
+  // useEffect(function () {
+  //   init()
+  // }, [])
+
+  // async function init() {
+  //   const url = ''
+  //   const data = await fetch(url).then(r=>r.json)
+  //   setProducts(data)
+  // }
+
   return(
     <>
       <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="10000">
@@ -17,10 +33,16 @@ function ShopPage() {
             </div>
           </div>
           <div class="carousel-item" data-bs-interval="2000">
-            <img src="https://robbreport.com/wp-content/uploads/2020/09/https___hypebeast.jpg?w=1000" class="d-block w-100" alt="chair"/>
+            <img src="https://i.gyazo.com/cc114cc01d65fa4820be8652151b61b0.png" class="d-block w-100" alt="chair"/>
             <div class="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
+              <h5>Ergonomic Chair</h5>
               <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img src="https://i.gyazo.com/97b764beb0a96af871fec590c7e217c3.png" class="d-block w-100" alt="chair"/>
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Featured Setup</h5>
             </div>
           </div>
         </div>
@@ -33,6 +55,9 @@ function ShopPage() {
           <span class="visually-hidden">Next</span>
         </button>
       </div>
+      {/* products.map( products => 
+        
+        ) */}
     </>
 
   )
