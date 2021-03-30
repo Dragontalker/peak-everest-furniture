7806d1f3bacdfd5fd18d70fb4489fc7d68998b37
jcxiao <chang.x1994@gmail.com>
2021-03-30 15:02:33 -0400

    Added to-dos to admin page

--- client/src/components/AdminPage/index.js
+++ client/src/components/AdminPage/index.js
@@ -14,7 +14,10 @@ function AdminPage() {
     <div className="admin-page card mt-3">
       <div className="card-body">
         <h5 className="card-title">Welcome Back [Admin]</h5>
-
+        <p>Add: sales charts</p>
+        <p>Add: banner info (?)</p>
+        <p>Add: product listing (btn: add/edit/remove product)</p>
+        <p>Add: transactions history (btn: sold/cancel transaction)</p>
       </div>
     </div>
   )
