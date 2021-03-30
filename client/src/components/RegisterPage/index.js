bc181fd4ad58218db7d81aca46ab8c34c368ed61
Siyuan Huo <georgehuo0211@gmail.com>
2021-03-30 14:56:22 -0400

    make RegisterPage a state, check email format, check if psw match

--- client/src/components/RegisterPage/index.js
+++ client/src/components/RegisterPage/index.js
@@ -1,38 +1,85 @@
-function RegisterPage() {
+import {React, Component} from 'react';
 
-  function submitForm(e) {
-    e.preventDefault();
-    console.log("attempting to sign up");
+
+class RegisterPage extends Component {
+  // state
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    repassword: ''
+  }
+  // functions
+  handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
   }
 
-  return(
-    <div className="card mt-3 login-body">
-      <div className="card-body">
-        <h5 className="card-title mb-3">Sign Up</h5>
-        <form onSubmit={e => submitForm(e)}>
-          <div className="mb-3">
-            <label htmlFor="displayNameInput" className="form-label">Display name</label>
-            <input type="text" className="form-control" id="displayNameInput" placeholder="name" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="emailInput" className="form-label">Email</label>
-            <input type="email" className="form-control" id="emailInput" placeholder="name@example.com" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="passwordInput" className="form-check-label" >Password</label>
-            <input type="password" className="form-control" id="passwordInput" placeholder="********" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="passwordReinput" className="form-check-label" >Confirm Password</label>
-            <input type="password" className="form-control" id="passwordReinput" placeholder="********" />
-          </div>
-          <div className="text-center mb-3">
-            <button className="btn btn-dark">Register</button>
-          </div>
-        </form>
+  checkValidateEmail = () => {
+    const {email} = this.state
+    const regex = new RegExp('.+@.+\\..+')
+    if (!regex.test(email)) {
+      alert('Invalid email format')
+    }
+  }
+
+  checkValidatePassword = () => {
+    const {password, repassword} = this.state
+    if (password !== repassword) {
+      alert("Password does not match")
+      return false
+    }
+    return true
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+    console.log("attempting to sign up");
+    this.checkValidateEmail()
+    if (this.checkValidatePassword()) {
+      console.log('psw ok')
+    }
+  }
+
+  // render
+  render() {
+    return(
+      <div className="card mt-3 login-body">
+        <div className="card-body">
+          <h5 className="card-title mb-3">Sign Up</h5>
+          <form onSubmit={(e) => this.submitForm(e)}>
+            <div className="mb-3">
+              <label htmlFor="displayNameInput" className="form-label">Display name</label>
+              <input name='name' type="text" className="form-control" id="displayNameInput" placeholder="name" onChange={this.handleChange}/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="emailInput" className="form-label">Email</label>
+              <input name='email' type="email" className="form-control" id="emailInput" placeholder="name@example.com" onChange={this.handleChange}/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordInput" className="form-check-label" >Password</label>
+              <input name='password' type="password" className="form-control" id="passwordInput" placeholder="********" onChange={this.handleChange}/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordReinput" className="form-check-label" >Confirm Password</label>
+              <input name='repassword' type="password" className="form-control" id="passwordReinput" placeholder="********" onChange={this.handleChange}/>
+            </div>
+            <div className="text-center mb-3">
+              <button className="btn btn-dark">Register</button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+
+
+
 }
+
+
 
 export default RegisterPage;
