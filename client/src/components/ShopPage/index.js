97f0ed54ae797df55aa63249317a8f5ef53f0385
Michael Wong <michael.w228@gmail.com>
2021-03-30 13:38:13 -0400

    Updated shop-card display

--- client/src/components/ShopPage/index.js
+++ client/src/components/ShopPage/index.js
@@ -2,6 +2,21 @@ import Carousel from "./Carousel"
 import "./shop.css"
 // import { useEffect, useState } from 'react'
 
+const products = [{
+  id: 1,
+  name: "chair",
+  description: "sample text",
+  price: 100,
+  img: "https://i.gyazo.com/cc114cc01d65fa4820be8652151b61b0.png"
+},
+{  id: 2,
+  name: "desk",
+  description: "sample text",
+  price: 200,
+  img: "https://www.hermanmiller.com/content/dam/hmicom/page_assets/products/categories/gaming/it_cmp_gaming_motia_gaming_table.jpg"
+}
+]
+
 function ShopPage() {
   // const [products, setProducts] = useState([])
 
@@ -22,23 +37,15 @@ function ShopPage() {
       <div className="all-products-list mb-3">
         <h1>All Products</h1>
           <div className="row">
-            <div className="card">
-              <img className="card-image" alt="product"/>
+            {products.map( (product, idx) => 
+            <div key={idx} className="card shop-card">
+              <img src={product.img} className="shop-card-img" alt="product"/>
               <div className="card-body">
-                Hello
+                <h5 class="card-title">{product.name}</h5>
+                <p class="card-text"> {product.description}</p>
+                <p class="card-text"> {product.price}</p>
               </div>
-            </div>
-            {/* products.map( (product, idx) => )
-            <div key={idx} className="card">
-              <img className="card-image" alt="product"/>
-              <div className="card-body">
-                Hello
-                <h5 class="card-title">product.name</h5>
-                <p class="card-text"> product.description</p>
-                <p class="card-text"> product.price</p>
-                *
-              </div>
-            </div>*/}
+            </div>)}
           </div>
       </div>
     </div>
