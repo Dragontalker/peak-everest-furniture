fbfa05752eaccde71743cf26e6fa773dfa8e9403
jcxiao <chang.x1994@gmail.com>
2021-03-30 10:23:52 -0400

    Added base template for product page

--- client/src/components/ProductPage/index.js
+++ client/src/components/ProductPage/index.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
+
 function ProductPage() {
+
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    console.log(window.location.pathname);
+    // productID in the pathname ^, API call DB for product info
+    setProduct({name:"product name"});
+  },[])
+
   return(
     <div>
-      Product Page
+      Product Page: {product.name}
     </div>
   )
 }
