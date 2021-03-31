fe779979b5b19fc7e5319fd4e176d68d8fe2ee9a
Michael Wong <michael.w228@gmail.com>
2021-03-31 11:29:44 -0400

    card data is now pulling from /api/products

--- client/src/components/ShopPage/index.js
+++ client/src/components/ShopPage/index.js
@@ -1,21 +1,21 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./shop.css"
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
-const products = [{
-  id: 1,
-  name: "chair",
-  description: "sample text",
-  price: 100,
-  img: "https://i.gyazo.com/cc114cc01d65fa4820be8652151b61b0.png"
-},
-{  id: 2,
-  name: "desk",
-  description: "sample text",
-  price: 200,
-  img: "https://www.hermanmiller.com/content/dam/hmicom/page_assets/products/categories/gaming/it_cmp_gaming_motia_gaming_table.jpg"
-}]
+// const products = [{
+//   id: 1,
+//   name: "chair",
+//   description: "sample text",
+//   price: 100,
+//   img: "https://i.gyazo.com/cc114cc01d65fa4820be8652151b61b0.png"
+// },
+// {  id: 2,
+//   name: "desk",
+//   description: "sample text",
+//   price: 200,
+//   img: "https://www.hermanmiller.com/content/dam/hmicom/page_assets/products/categories/gaming/it_cmp_gaming_motia_gaming_table.jpg"
+// }]
 
 const bannerData = [
   {img:"https://www.hermanmiller.com/content/dam/hmicom/page_assets/products/categories/gaming/it_cmp_gaming_motia_gaming_table.jpg"},
@@ -24,17 +24,18 @@ const bannerData = [
 ]
 
 function ShopPage() {
-  // const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([])
 
-  // useEffect(function () {
-  //   init()
-  // }, [])
+  useEffect(function () {
+    init()
+  }, [])
 
-  // async function init() {
-  //   const url = ''
-  //   const data = await fetch(url).then(r=>r.json)
-  //   setProducts(data)
-  // }
+  async function init() {
+    const url = '/api/products'
+    const data = await fetch(url).then(r=>r.json())
+    console.log(data)
+    setProducts(data)
+  }
 
   return(
     <div className="shop-page mt-3">
@@ -55,14 +56,14 @@ function ShopPage() {
       <div className="all-products-list mb-3">
         <h1>All Products</h1>
           <div className="row">
-            {products.map((product, idx) => 
+            {products && products.map((product, idx) => 
               <div key={idx} className="col-12 col-sm-6 col-md-4 col-lg-3">
                 <div className="card">
                   <div className="shop-card-img">
-                    <img src={product.img} className="card-img-top img-fluid" alt={product.name} />
+                    <img src={product.image} className="card-img-top img-fluid" alt={product.heading} />
                   </div>
                   <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
+                    <h5 className="card-title">{product.heading}</h5>
                     <p className="card-text"> {product.description}</p>
                     <p className="card-text"> ${product.price}</p>
                     <a href={"/product/"+product.id} className="stretched-link">See Product</a>
