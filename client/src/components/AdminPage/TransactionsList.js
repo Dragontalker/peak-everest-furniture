8f03a41d6e5d28060d7d7507c130c8e882b6fe51
jcxiao <chang.x1994@gmail.com>
2021-03-31 20:44:16 -0400

    Added timestamp to transaction listing

--- client/src/components/AdminPage/TransactionsList.js
+++ client/src/components/AdminPage/TransactionsList.js
@@ -1,31 +1,47 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function TransactionsList(props) {
+
+  const [trans, setTrans] = useState([]);
+
+  useEffect(() => {
+    // remove transactions that are marked BUYING
+    let transData = props.trans.filter(entry => entry.status === "BOUGHT");
+    // extend trans with product data
+    transData.forEach(entry => {
+      props.products.forEach(product => {
+        if (product.id === entry.productid) entry.productname = product.heading;
+      })
+    });
+    setTrans(transData);
+  },[props])
 
   return(
     <div className="admin-trans-list table-responsive">
       <h5>Ongoing Transactions</h5>
       <table className="table table-sm">
         <thead>
-            <tr>
-              <th scope="col">transaction ID</th>
-              <th scope="col">User ID</th>
-              <th scope="col">Product ID</th>
-              <th scope="col">Product Name</th>
-              <th scope="col">Time of sale</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>123</td>
-              <td>asdjklebnkl</td>
-              <td>1203-2344</td>
-              <th>Graco SnugRide</th>
-              <td>Mar 30, 2021 10:45:23</td>
+          <tr>
+            <th scope="col">Transaction ID</th>
+            <th scope="col">User ID</th>
+            <th scope="col">Product ID</th>
+            <th scope="col">Product Name</th>
+            <th scope="col">Time of sale</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {trans.map(entry => 
+            <tr key={entry.id}>
+              <td>{entry.id}</td>
+              <td>{entry.userid}</td>
+              <td>{entry.productid}</td>
+              <th>{entry.productname}</th>
+              <td>--/--/----</td>
               <td className="text-end"><button className="btn btn-sm btn-outline-dark">Delivered</button></td>
             </tr>
-          </tbody>
+          )}
+        </tbody>
       </table>
     </div>
   )
