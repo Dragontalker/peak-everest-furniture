d8a0960156a6ee33c9c4a9236e891557d7a11555
jcxiao <chang.x1994@gmail.com>
2021-04-01 11:55:44 -0400

    Added placeholder functions for shop cart

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -22,27 +22,31 @@ function ShoppingCart() {
   }, [store.openShopCart])
 
   useEffect(() => {
-    // fetch cartData using userid
-    setCartData([
-      {id:"123a", productid:"1203-2343", price:10.95, heading:"Bosch Refrigerator", transactionid:"111"},
-      {id:"321b", productid:"1203-2344", price:12.45, heading:"Graco SnugRide", transactionid:"222"}
-    ]);
-  }, [])
+    if (store.loggedIn) {
+      // fetch cartData using userid
+      console.log("fetching cart data");
+      setCartData([
+        {id:"123a", productid:"1203-2343", price:10.95, heading:"Bosch Refrigerator", transactionid:"111"},
+        {id:"321b", productid:"1203-2344", price:12.45, heading:"Graco SnugRide", transactionid:"222"}
+      ]);
+    }
+  }, [store.loggedIn])
 
-  // async function handleCheckout(id) {
-  //   console.log("checkout shopping cart item", id);
-  //   // SET transaction status to "BOUGHT"
-  //   // REMOVE entry from shopping cart
-  //   // FETCH new shoppingcart
-  //   setCartData([]);
-  // }
+  async function handleCheckout() {
+    console.log("checkout all shopping cart items");
+    // SET transaction status to "BOUGHT"
+    // REMOVE entry from shopping cart
+    // FETCH new shoppingcart
+    setCartData([]);
+  }
 
   async function handleCancel(id) {
     console.log("cancel shopping cart item", id);
     // SET transaction status to "CANCELLED"
     // REMOVE entry from shopping cart
     // FETCH new shoppingcart
-    setCartData([]);
+    let newCart = cartData.filter(entry => entry.id !== id);
+    setCartData(newCart);
   }
 
   function calculateTotal() {
@@ -58,22 +62,26 @@ function ShoppingCart() {
       <div className="shop-cart-body" ref={shoppingCart}>
         <h4 className="mt-2 mb-3">Shopping Cart</h4>
         <ul className="list-group">
+          {cartData.length ? '' : <span className="text-muted">No items in your cart </span> }
           {cartData.map(item => 
             <li className="list-group-item d-flex shop-cart-item" key={item.id}>
               <div className="flex-fill ms-2 mt-1" style={{transform:"rotate(0)"}}>
-                <h5 className="card-text">
+                <p className="card-text">
                   {item.heading.length < 23 ? item.heading : item.heading.slice(0,22) + "..."}
-                </h5>
+                </p>
                 <Link className="stretched-link" to={"/product/"+item.productid} 
                   onClick={() => setStore({type:"toggle-shop-cart"})}>Go to product page</Link>
               </div>
-              <button className="btn btn-danger" onClick={() => handleCancel(item.id)}>Cancel</button>
+              <div className="mt-auto">
+                <p className="text-end me-2">${item.price}</p>
+                <button className="btn btn-sm link-danger" onClick={() => handleCancel(item.id)}>Remove</button>
+              </div>
             </li>
           )}
         </ul>
         <div className="checkout-container">
           <p>Total: ${calculateTotal()} </p>
-          <button className="btn btn-light">Checkout</button>
+          <button className="btn btn-light" onClick={handleCheckout}>Checkout</button>
         </div>
         
       </div>
